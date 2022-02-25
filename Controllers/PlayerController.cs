fc0a7ebf69dffc9e586b7c6de88980798d9353aa
RajatVerma1309 <rajatverma1309@gmail.com>
2022-02-25 18:13:02 +0530

    second commit

---
+++ Controllers/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication29.Models;
+
+namespace WebApplication29.Controllers
+{
+    public class PlayerController : Controller
+    {
+        // GET: Player
+        public ActionResult Index()
+        {
+            List<Player> list = new List<Player>
+            {
+                new Player{PID =1,PName="Ram",PTeam="KKR"},
+                 new Player{PID =2,PName="Sam",PTeam="KKR"},
+            };
+            return View();
+        }
+    }
+}
