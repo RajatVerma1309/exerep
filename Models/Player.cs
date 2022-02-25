af46b1a6ebf7560e1e051a412d79b689607a7e30
RajatVerma1309 <rajatverma1309@gmail.com>
2022-02-25 18:08:48 +0530

    first comment

---
+++ Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication29.Models
+{
+    public class Player
+    {
+        public int PID { get; set; }
+
+        public string PName { get; set; }
+        public string PTeam { get; set; }
+    }
+}
